0c6d154e1551c4370f4f3e5bad2ac24263a49915
Stanislaw Baranski <stan@stan.bar>
2022-05-11 11:08:07 +0200

    add circuit generator

--- circuit_generator.py
+++ circuit_generator.py
@@ -1,6 +1,14 @@
 from typing import NamedTuple
 from random import random
 
+Id = int
+
+class Wire(NamedTuple):
+    value: int
+    label: int
+    select_bit: int
+
+
 class Gate(NamedTuple):
     id: int
     type: str
@@ -22,26 +30,42 @@ class Gate(NamedTuple):
         out = operator(self.inputs)
 
 
-
-class Wire(NamedTuple):
-    value: int
-    label: int
-    select_bit: int
-
 class Circuit():
     alice: list[int]
     bob: list[int]
     out: list[int]
     gates: list[Gate]
-    wires: set[Wire]
-    pbits: dict[Wire, int]
+    wires: list[Wire]
+    pbits: dict[Wire, int] = {}
+    keys: dict[Id, tuple[int,int]] = {}
+    garbled_tables: dict[Wire, int] = {}
 
-    def __init__(self, alice, bob, out, gates):
+    def __init__(self, alice: list[int], bob: list[int], out: list[int], gates: list[Gate], kappa: int):
+        self.alice = alice
+        self.bob = bob
+        self.out = out
+        self.gates = gates
+
+        self.populate_wires()
         self.generate_pbits()
+        self.generate_keys(kappa)
+
+
+    def populate_wires(self):
+        wires = set()
+        for gate in self.gates:
+            wires.add(gate.id)
+            wires.update(set(gate.inputs))
+        self.wires = list(wires)
+
+    
+    def generate_keys(self, kappa: int):
+        self.keys = {id: (random.getrandbits(kappa), random.getrandbits(kappa)) for (id) in self.wires}
+
 
 
     def generate_pbits(self):
-        self.pbits = {wire: random.randint(0, 1) for wire in self.wires}
+        self.pbits = {id: random.randint(0, 1) for (id) in self.wires}
 
 one_bit_comparator = Circuit(alice=[1], bob=[2], out=[3], gates=[Gate(3, "CMP", [1,2])])
 
